c1e264749fdf78de34d4675c17549598ca7ae196
Manuel Fonseca <manueldazafon@gmail.com>
2022-04-04 17:58:22 -0500

    add archive to about page

---
+++ src/pages/ecommerce/AboutScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutScreen = () => {
+    return (
+        <div>
+            <h1>Page to About</h1>
+        </div>
+    );
+};
+
+export default AboutScreen;
