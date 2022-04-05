5e66cb5b62eee4f88bb2886d823771e78c287f6a
Manuel Fonseca <manueldazafon@gmail.com>
2022-04-04 20:19:34 -0500

    add images about page and modified route about

--- src/routes/StoreRouter.js
+++ src/routes/StoreRouter.js
@@ -1,14 +1,16 @@
 import { Route, Routes } from "react-router-dom"
 import HomeScreen from "../pages/ecommerce/HomeScreen"
 import ProductScreen from "../pages/ecommerce/ProductScreen"
+import AboutScreen from '../pages/ecommerce/AboutScreen'
 
 
 const StoreRouter = () => {
     return (
         <>
             <Routes>
-                <Route path="product/:id" element={<ProductScreen />} />
-                <Route path="/*" element={<HomeScreen />} />
+                <Route path="product/:id" element={ <ProductScreen />} />
+                <Route path="/*" element={ <HomeScreen />} />
+                <Route path="about" element={ <AboutScreen /> } />
             </Routes>
         </>
       )
