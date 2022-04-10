921094c065da5957031fe37a4ad62f07a8172a66
Manuel Fonseca <manueldazafon@gmail.com>
2022-04-10 17:44:16 -0500

    agrego vista productoScreen

--- src/pages/ecommerce/ProductScreen.js
+++ src/pages/ecommerce/ProductScreen.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import ProductImages from '../../components/ecommerce/Product/ProductImages';
+import ProductoInfo from '../../components/ecommerce/Product/ProductoInfo';
+import ProductFeatures from '../../components/ecommerce/Product/ProductFeatures';
 
 const ProductScreen = () => {
-  return (
-    <div>ProductScreen</div>
-  )
-}
+    return (
+      <>
+        <section className="container_product_screen">
+          <ProductImages />
+          <ProductoInfo />
+        </section>
+        <section>
+          <ProductFeatures />
+        </section>
+      </>
+    );
+};
 
-export default ProductScreen
+export default ProductScreen;
