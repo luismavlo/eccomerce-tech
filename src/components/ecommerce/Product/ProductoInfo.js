58f347c9e27cac31f84fc13de6667e8fefec3a41
Manuel Fonseca <manueldazafon@gmail.com>
2022-04-12 18:09:02 -0500

    agrego funcionamiento para agregar al carrito

--- src/components/ecommerce/Product/ProductoInfo.js
+++ src/components/ecommerce/Product/ProductoInfo.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import AlertScreen from '../../ui/AlertScreen';
+import { startAddProductCart } from '../../../redux/actions/products';
+import { useForm } from 'react-hook-form';
+import { useSelector, useDispatch } from 'react-redux';
 
 const ProductoInfo = () => {
 
+    const dispatch = useDispatch();
+
     const { productDetail } = useSelector( state => state.products );
+    const { alertScreen } = useSelector( state => state.ui);
+
+    const { register, handleSubmit } = useForm();
+
+    const addToCart = (data) => {
+        const product = {
+            id: productDetail?.id,
+            quantity: parseInt(data?.quantity)
+        }
+
+        dispatch(startAddProductCart(product))
+            .then( () => <AlertScreen message="Se a agregado el producto al carrito" severity="success" />);
+    }
 
     return (
         <section className="container_product_info">
@@ -12,10 +30,16 @@ const ProductoInfo = () => {
             <p>Description</p>
             <p>{ productDetail.description }</p>
             <h5>Quantity</h5>
-            <form>
-                <input type="number" />
-                <button>ADD TO CART</button>
-            </form>
+                <form onSubmit={handleSubmit(addToCart)}>
+                    <input 
+                        type="number"
+                        {...register("quantity", { required: true })}
+                    />
+                    <button>ADD TO CART</button>
+                </form>
+                {
+                    alertScreen && <AlertScreen message="Se a agregado el producto al carrito" severity="success" />
+                }
         </section>
     );
 };
